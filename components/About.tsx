db6b18629ecf705f58b73a84a2aba201e79eaaec
as2811-project <anandh.tedxchennai@gmail.com>
2024-07-11 16:50:28 +1000

    Add link to resume in About section

--- components/About.tsx
+++ components/About.tsx
@@ -58,9 +58,14 @@ const AboutMe: React.FC = () => {
                 description="Pictures of people, just to see a smile on their faces :)"
               />
             </div>
-            <button className="rounded-full py-2 px-2 text-black bg-white hover:bg-neutral-700 hover:text-white">
-              My Resume
-            </button>
+            <a
+              href="https://lavulopbfiogsnnczeae.supabase.co/storage/v1/object/public/photos/Resume_Anandh.pdf?t=2024-07-11T06%3A48%3A55.976Z"
+              target="_blank"
+            >
+              <button className="rounded-full py-2 px-2 text-black bg-white hover:bg-neutral-700 hover:text-white">
+                My Resume
+              </button>
+            </a>
           </div>
         </div>
       </div>
